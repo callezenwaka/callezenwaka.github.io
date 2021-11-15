194ea7e05546626ba8f5ab7f24210688c57a616e
callezenwaka <callisezenwaka@gmail.com>
2021-11-15 22:15:34 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+let footerContent = document.querySelector('#footer--content')
+console.log(footerContent)
+footerContent.innerHTML += new Date().getFullYear();
+// element.innerHTML += "additional HTML code"
